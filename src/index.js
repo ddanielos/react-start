b312c929d18e38eb7f3e5ef3277987977f990b7f
Dany Daniel <68875920+ddanielos@users.noreply.github.com>
2022-04-20 22:44:22 -0500

    update createRoot replace
    En nuevas versiones de react se cambia el ReactDOM.render por createRoot

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+//import ReactDOM from "react-dom"; 
+//es reemplazado en nuevas versiones por:
+import { createRoot } from "react-dom/client"
+
 import App from './containers/App';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+//ReactDOM.render(<App />, document.getElementById('app'));
+//Reemplazado en nuevas versiones por:
+const root = createRoot(document.getElementById('app'))
+root.render(<App />)
